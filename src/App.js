686c5a63c6c2899d9fd5340979f02309c8a42f36
DC <david.chamberlin@tadashishoji.com>
2020-01-20 14:22:27 -0800

    frontend: installed sass, completed header + replaced navbar component -- next: complete page components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import logo from './logo.svg';
 import './App.css';
 
-import Navbar from './components/navbar.component.js';
+import Header from './components/header.component.js';
 // import Info from './components/info.component.js';
 // import Discography from './components/discography.component.js';
 // import Gallery from './components/gallery.component.js';
@@ -17,21 +17,64 @@ import Navbar from './components/navbar.component.js';
 function App() {
   return (
     <Router>
-      <div className='container'>
-        <Navbar />
-        <br />
-        {/*
-        <Route path='/' exact component={Info} />
-        <Route path='/edit/:id' exact component={Discography} />
-        <Route path='/create' exact component={Gallery} />
-        <Route path='/user' exact component={Shop} />
-        <Route path='/user' exact component={Reviews} />
-        <Route path='/user' exact component={Lyrics} />
-        <Route path='/user' exact component={Contact} />
-        */}
+      <div className="container--outer-wrapper">
+        <div className="overlay--outer-wrapper">
+
+        </div>
+        <div className="container--inner-wrapper">
+          {/* refactor as component later */}
+          {/*}
+          <nav id="main-nav" className="mobile">
+            <ul className="mobile list balance_4 top menu-length-8">
+              <li className="top">
+                <div><a className="top" href="/home">Home</a></div>
+              </li>
+              <li className="top">
+                <div><a className="top" href="/info">Info</a></div>
+              </li>
+              <li className="top">
+                <div><a className="top" href="/discography">Discography</a></div>
+              </li>
+              <li className="top">
+                <div><a className="top" href="/gallery">Gallery</a></div>
+              </li>
+              <li className="top">
+                <div><a className="top" href="/shop">Shop</a></div>
+              </li>
+              <li className="top">
+                <div><a className="top" href="/reviews">Reviews</a></div>
+              </li>
+              <li className="top">
+                <div><a className="top" href="/lyrics">Lyrics</a></div>
+              </li>
+              <li className="top">
+                <div><a className="top" href="/contact">Contact</a></div>
+              </li>
+            </ul>
+          </nav>
+          */}
+
+          <div className="main-content">
+            <div id="container" className="container">
+          
+              <Header />
+              <br />
+              {/*
+                <Route path='/' exact component={Info} />
+                <Route path='/edit/:id' exact component={Discography} />
+                <Route path='/create' exact component={Gallery} />
+                <Route path='/user' exact component={Shop} />
+                <Route path='/user' exact component={Reviews} />
+                <Route path='/user' exact component={Lyrics} />
+                <Route path='/user' exact component={Contact} />
+              */}
+            </div>
+          </div>
+        </div>
+          
       </div>
     </Router>
-  );
-}
-
-export default App;
+          );
+        }
+        
+        export default App;
---
+++ src/components/header.component.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import '../assets/css/main.scss';
+
+export default class Header extends Component {
+
+    render() {
+        return (
+            <header id="page-header">
+                <div className="spacer--header">
+
+                </div>
+                <div className="nav-bar">
+                    <div className="site-content-wrap">
+                        <span id="toggle-mobile-menu" className="toggle-mobile-menu">
+                            <a href="javascript:">
+                                <i className="icon-hamburger"></i>
+                            </a>
+                        </span>
+                        <nav id="main-nav" className="desktop">
+                            <ul className="">
+                                <li>
+                                    <div>
+                                        <a href="/">Home</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div>
+                                        <a href="/info">Info</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div>
+                                        <a href="/discography">Discography</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div>
+                                        <a href="/gallery">Gallery</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div>
+                                        <a href="/shop">Shop</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div>
+                                        <a href="/review">Reviews</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div>
+                                        <a href="/lyrics">Lyrics</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div>
+                                        <a href="/contact">Contact</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
