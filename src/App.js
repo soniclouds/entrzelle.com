04f5650fb239e740497b0e597efacced3e739e65
DC <david.chamberlin@tadashishoji.com>
2020-01-20 13:10:40 -0800

    frontend: installed bootstrap + react-router-dom, created initial navbar, created placeholder components for pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './components/navbar.component.js';
+// import Info from './components/info.component.js';
+// import Discography from './components/discography.component.js';
+// import Gallery from './components/gallery.component.js';
+// import Shop from './components/shop.component.js';
+// import Reviews from './components/reviews.component.js';
+// import Lyrics from './components/lyrics.component.js';
+// import Contact from './components/contact.component.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='container'>
+        <Navbar />
+        <br />
+        {/*
+        <Route path='/' exact component={Info} />
+        <Route path='/edit/:id' exact component={Discography} />
+        <Route path='/create' exact component={Gallery} />
+        <Route path='/user' exact component={Shop} />
+        <Route path='/user' exact component={Reviews} />
+        <Route path='/user' exact component={Lyrics} />
+        <Route path='/user' exact component={Contact} />
+        */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/navbar.component.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+    render() {
+        return (
+            <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
+                {/* <Link to='/' className='navbar-brand'>Exercise Tracker</Link> */}
+                <div className='collapse navbar-collapse'>
+                    <ul className='navbar-nav mr-auto'>
+                        <li className='navbar-item'>
+                            <Link to='/' className='nav-link'>Home</Link>
+                        </li>
+                        <li className='navbar-item'>
+                            <Link to='/info' className='nav-link'>Info</Link>
+                        </li>
+                        <li className='navbar-item'>
+                            <Link to='/discography' className='nav-link'>Discography</Link>
+                        </li>
+                        <li className='navbar-item'>
+                            <Link to='/gallery' className='nav-link'>Gallery</Link>
+                        </li>
+                        <li className='navbar-item'>
+                            <Link to='/shop' className='nav-link'>Shop</Link>
+                        </li>
+                        <li className='navbar-item'>
+                            <Link to='/reviews' className='nav-link'>Reviews</Link>
+                        </li>
+                        <li className='navbar-item'>
+                            <Link to='/lyrics' className='nav-link'>Lyrics</Link>
+                        </li>
+                        <li className='navbar-item'>
+                            <Link to='/contact' className='nav-link'>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
