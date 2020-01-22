179aa8b546d3ef7b50fcd035811113a42d73ebbb
DC <david.chamberlin@tadashishoji.com>
2020-01-21 17:55:39 -0800

    checkpoint: in the process of designing home layout + cleaning up FB embed

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -12,42 +12,51 @@ export default class Home extends Component {
         js = d.createElement(s); js.id = id;
         js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5&appId=744521848981682";
         fjs.parentNode.insertBefore(js, fjs);
-        
-
-
+    
     }
 
 
     render() {
         return (
-          <div>
-            
-            <div id="fb-root">&nbsp;</div>
-                
-                {this.embedFacebookTimeline(document, 'script', 'facebook-jssdk')}
+            <div id='content-wrap'>
+                <div id='content' className='row'>
+                    <div class='col-4 home-columns'>
+                        
+                        
+                        <div id="fb-root">&nbsp;</div>
+                        
+                            {this.embedFacebookTimeline(document, 'script', 'facebook-jssdk')}
 
-                <div className="fb-page" data-adapt-container-width="true" data-height="90000" data-hide-cover="true"
-                    data-href="https://www.facebook.com/entrzelle/" data-show-facepile="true" data-small-header="false"
-                    data-tabs="timeline" data-width="500">
-                    <div className="fb-xfbml-parse-ignore">
-                        <blockquote cite="https://www.facebook.com/entrzelle/">
-                            <a href="https://www.facebook.com/entrzelle/">ENTRZELLE</a>
-                        </blockquote>
+                        <div className="fb-page" data-adapt-container-width="true" data-height="90000" data-hide-cover="true"
+                            data-href="https://www.facebook.com/entrzelle/" data-show-facepile="true" data-small-header="false"
+                            data-tabs="timeline" data-width="500">
+                            <div className="fb-xfbml-parse-ignore">
+                                <blockquote cite="https://www.facebook.com/entrzelle/">
+                                    <a href="https://www.facebook.com/entrzelle/">ENTRZELLE</a>
+                                </blockquote>
+                            </div>
+                        </div>
+                        <div className="fb-page" data-adapt-container-width="true" data-height="90000" data-hide-cover="true"
+                            data-href="https://www.facebook.com/entrzelle/" data-show-facepile="true" data-small-header="false" data-tabs="events"
+                            data-width="500">
+                            <div className="fb-xfbml-parse-ignore">
+                                <blockquote cite="https://www.facebook.com/entrzelle/">
+                                    <a href="https://www.facebook.com/entrzelle/">ENTRZELLE</a>
+                                </blockquote>
+                            </div>
+                        </div>
+                        
                     </div>
-                </div>
-                <div className="fb-page" data-adapt-container-width="true" data-height="90000" data-hide-cover="true"
-                    data-href="https://www.facebook.com/entrzelle/" data-show-facepile="true" data-small-header="false" data-tabs="events"
-                    data-width="500">
-                    <div className="fb-xfbml-parse-ignore">
-                        <blockquote cite="https://www.facebook.com/entrzelle/">
-                            <a href="https://www.facebook.com/entrzelle/">ENTRZELLE</a>
-                        </blockquote>
+
+                    <div class='col-4 home-columns'>
                     </div>
+                    
+                    <div class='col-4 home-columns'>
+                    </div>
+
                 </div>
 
-
-
-          </div>
+            </div>
         )
     }
 }
