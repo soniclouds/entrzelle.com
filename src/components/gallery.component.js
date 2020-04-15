f0b4150bdf4bc386c7d1256b848ac6a1548394ea
DC <david.chamberlin@tadashishoji.com>
2020-04-15 16:02:52 -0700

    test: added test assets + markup for gallery component

--- src/components/gallery.component.js
+++ src/components/gallery.component.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 
+import '../assets/css/gallery.scss';
+
+import _image1_thumb from '../assets/img/photos/gallery/thumbs/200/promo_band_1.jpg';
+import _image2_thumb from '../assets/img/photos/gallery/thumbs/200/promo_band_2.jpg';
+
+import _image1_full from '../assets/img/photos/gallery/full/promo_band_1.jpg';
+import _image2_full from '../assets/img/photos/gallery/full/promo_band_2.jpg';
+
+
 export default class Gallery extends Component {
 
     photos = [
-        {
-            src: 'https://imagery.zoogletools.com/u/64729/42f521f95519635214049aea191f2999506a2674/photo/postcard1-b.jpg',
+        {   
+            src: {
+                // full: 'https://imagery.zoogletools.com/u/64729/42f521f95519635214049aea191f2999506a2674/photo/postcard1-b.jpg',
+                thumb: _image1_thumb,
+                full: _image1_full
+            },
             // width: 1,
             // height: 1
         },
         {
-            src: 'https://imagery.zoogletools.com/u/64729/647d463667a5b6947999c2ecc587dcf35aaeb9fd/photo/1c.jpg',
+            src: {
+                // full: 'https://imagery.zoogletools.com/u/64729/42f521f95519635214049aea191f2999506a2674/photo/postcard1-b.jpg',
+                thumb: _image2_thumb,
+                full: _image2_full
+            },
             // width: 1,
             // height: 1
-        }
+        },
+        
     ];
 
 
@@ -20,7 +38,15 @@ export default class Gallery extends Component {
         return (
             <div>
 
-                {/* <Gallery photos={this.photos} />; */}
+                <div className="gallery-photos">
+                    <a href={this.photos[0].src.full} >
+                        <img src={this.photos[0].src.thumb} alt="test-thumb-1"/>
+                    </a>
+                    <a href={this.photos[1].src.full}>
+                        <img src={this.photos[1].src.thumb} alt="test-thumb-2"/>
+                    </a>
+                    {/* {images.map(({ id, src, title }) => <img key={id} src={src} title={title} alt={title} />)} */}
+                </div>
                 
             </div>
         )
---
+++ src/components/images.js
@@ -0,0 +1,8 @@
+const images = [
+    {
+        id: 1, 
+        src: '../assets/img/photos/gallery/thumbs/200/promo_band_1.jpg'
+    }
+]
+
+export default images;
