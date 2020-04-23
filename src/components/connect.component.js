eb2791fddaca7e18f234daeb6a60b11daab03d06
DC <david.chamberlin@tadashishoji.com>
2020-04-23 13:10:18 -0700

    fixed css and js to display connect modal correctly on toggle

--- src/components/connect.component.js
+++ src/components/connect.component.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
  
+import '../assets/css/connect.scss';
+
 export default class Connect extends Component {
  
+    componentDidMount(){
+        this.setContainerHeight();
+        window.addEventListener('resize', this.setContainerHeight);
+    }
+
+    setContainerHeight() {
+        // let contentTop = document.getElementById('connect--outer').offsetTop,
+        let contentTop = document.getElementById('ccc-outer').offsetTop,
+            contentContainer = document.getElementById('connect--outer');
+
+        contentContainer.style.height = window.innerHeight - contentTop - '50' + 'px';
+        // audioCol.style.height = window.innerHeight - contentTop + 'px';
+
+    }
 
 
     render() {
         return (
             <div id="connect-component-container">
-                <div id="ccc-outer" className="component-content-container--outer">
+                <div id="connect--outer" className="--outer">
 
-                    <div className="component-content-container--inner">
+                    {/* <div className="component-content-container--inner"> */}
                         connect component contents here
-                    </div>
+                    {/* </div> */}
                 
                 </div>
             </div>
