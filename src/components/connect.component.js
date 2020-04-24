2cc06fc71d0c8875e638cf8460a7a7d422b9740b
DC <david.chamberlin@tadashishoji.com>
2020-04-24 16:55:25 -0700

    cta positioning fixed

--- src/components/connect.component.js
+++ src/components/connect.component.js
@@ -35,49 +35,50 @@ export default class Connect extends Component {
 
                     <div className="connect-cta-container">
                         <div className="--inner">
-                            
-                            <div className='cta-links-header row'>
-                                <span className='col-sm-6'>connect socially</span>
-                                <span className='col-sm-6'>stream music</span>
-                            </div>
-                            
-                            <div className="cta-links-container row">
-                                <div className="--social-links col-sm-6">
-                                    <a className='fa-facebook' href="https://www.facebook.com/entrzelle"><span>facebook</span></a>
-                                    <a className='fa-twitter' href="https://www.twitter.com/entrzelle"><span>twitter</span></a>
-                                    <a className='fa-youtube' href="https://www.youtube.com/user/Entrzelle"><span>youtube</span></a>
+                            <div className="--inner-inner">
+                                <div className='cta-links-header row'>
+                                    <span className='col-sm-6'>connect socially</span>
+                                    <span className='col-sm-6'>stream music</span>
                                 </div>
-                                <div className="--music-links col-sm-6">
-                                    <a className='fa-spotify' href="https://open.spotify.com/artist/7E07y9odlPu5zJcm8zjhkd"><span>spotify</span></a>
-                                    <a className='fa-reverbnation' href="https://www.reverbnation.com/entrzelle"><span>reverbnation</span></a>
-                                    <a className='fa-soundcloud' href="https://soundcloud.com/entrzelle"><span>soundcloud</span></a>
-                                </div>
-{/* 
-                                <div className="--label-links col-sm-4">
-                                    <a href="https://alfamatrix.bandcamp.com/">alfamatrix-bandcamp</a>
-                                    <a href="https://alfa-matrix-store.com/">alfamatrix-website-store</a>
-                                    <a href="http://www.alfa-matrix.com/">alfamatrix-website-home</a>
-                                </div>
-                                 */}
-                            </div>
-                            
-                            <div className='cta-links-header row'>
-                                <span className='col-sm-12'>email me</span>
-                            </div>
-                            
-                            <div className="cta-email-container row">
-                                <div className="--email-directly col-sm-12">
-                                    <a className='fa-email' href="mailto:entrzelle@gmail.com"><span style={{display: 'none'}}>email me</span></a>
-                                </div>
-                            </div>
-
-                            <div className='cta-links-header row'>
-                                <span className='col-sm-12'>label info</span>
-                            </div>
                                 
-                            <div className="cta-label-container row">
-                                <div className="--label col-sm-12">
-                                    <a className='fa-label' href="https://alfa-matrix-store.com/"><span style={{display: 'none'}}>label info</span></a>
+                                <div className="cta-links-container row">
+                                    <div className="--social-links col-sm-6">
+                                        <a className='fa-facebook' href="https://www.facebook.com/entrzelle"><span>facebook</span></a>
+                                        <a className='fa-twitter' href="https://www.twitter.com/entrzelle"><span>twitter</span></a>
+                                        <a className='fa-youtube' href="https://www.youtube.com/user/Entrzelle"><span>youtube</span></a>
+                                    </div>
+                                    <div className="--music-links col-sm-6">
+                                        <a className='fa-spotify' href="https://open.spotify.com/artist/7E07y9odlPu5zJcm8zjhkd"><span>spotify</span></a>
+                                        <a className='fa-reverbnation' href="https://www.reverbnation.com/entrzelle"><span>reverbnation</span></a>
+                                        <a className='fa-soundcloud' href="https://soundcloud.com/entrzelle"><span>soundcloud</span></a>
+                                    </div>
+    {/* 
+                                    <div className="--label-links col-sm-4">
+                                        <a href="https://alfamatrix.bandcamp.com/">alfamatrix-bandcamp</a>
+                                        <a href="https://alfa-matrix-store.com/">alfamatrix-website-store</a>
+                                        <a href="http://www.alfa-matrix.com/">alfamatrix-website-home</a>
+                                    </div>
+                                    */}
+                                </div>
+                                
+                                <div className='cta-links-header row'>
+                                    <span className='col-sm-12'>email me</span>
+                                </div>
+                                
+                                <div className="cta-email-container row">
+                                    <div className="--email-directly col-sm-12">
+                                        <a className='fa-email' href="mailto:entrzelle@gmail.com"><span style={{display: 'none'}}>email me</span></a>
+                                    </div>
+                                </div>
+
+                                <div className='cta-links-header row'>
+                                    <span className='col-sm-12'>label info</span>
+                                </div>
+                                    
+                                <div className="cta-label-container row">
+                                    <div className="--label col-sm-12">
+                                        <a className='fa-label' href="https://alfa-matrix-store.com/"><span style={{display: 'none'}}>label info</span></a>
+                                    </div>
                                 </div>
                             </div>
 
