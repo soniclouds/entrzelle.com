a459bcf7856cb7966d21fc733ac024fa48f1cede
DC <david.chamberlin@tadashishoji.com>
2020-04-23 17:48:40 -0700

    added placeholder content to cta + applied initial css

--- src/components/connect.component.js
+++ src/components/connect.component.js
@@ -35,7 +35,30 @@ export default class Connect extends Component {
 
                     <div className="connect-cta-container">
                         <div className="--inner">
-                            cta here
+                            <div className="cta-links-container row">
+                                <div className="--social-links col-sm-4">
+                                    <a href="https://www.facebook.com/entrzelle">facebook</a>
+                                    <a href="https://www.twitter.com/entrzelle">twitter</a>
+                                    <a href="https://www.youtube.com/user/Entrzelle">youtube</a>
+                                </div>
+                                <div className="--music-links col-sm-4">
+                                    <a href="https://open.spotify.com/artist/7E07y9odlPu5zJcm8zjhkd">spotify</a>
+                                    <a href="https://www.reverbnation.com/entrzelle">reverbnation</a>
+                                    <a href="https://soundcloud.com/entrzelle">soundcloud</a>
+                                </div>
+                                <div className="--label-links col-sm-4">
+                                    <a href="https://alfamatrix.bandcamp.com/">alfamatrix-bandcamp</a>
+                                    <a href="https://alfa-matrix-store.com/">alfamatrix-website-store</a>
+                                    <a href="http://www.alfa-matrix.com/">alfamatrix-website-home</a>
+                                </div>
+                            </div>
+                            <div className="cta-email-container row">
+                                <div className="--email-directly col">
+                                    <a href="mailto:entrzelle@gmail.com">email me directly</a>
+                                </div>
+                            </div>
+
+
                         </div>
                     </div>
 
