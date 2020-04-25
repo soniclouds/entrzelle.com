8c4092369021689457d464ca0b4e84bf153f7129
DC <david.chamberlin@tadashishoji.com>
2020-04-24 19:50:48 -0700

    commented out gallery code in home component in favor of slideshow/carousel -- provided temporary image placeholder as background for 'component-content-container--inner' -- next: implement slideshow/carousel

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -3,16 +3,23 @@ import React, { Component } from 'react';
 import CTA from './connect/cta.js';
 // import FB from './connect/fb.js';
 // import Gallery from './gallery.component.js';
-import galleryPhotos from './gallery/images.js';
+// import galleryPhotos from './gallery/images.js';
 
 import '../assets/css/gallery.scss';
 import '../assets/css/connect.scss';
 import '../assets/css/home.scss';
 import '../assets/css/_content-component.scss';
 
+// import 'lightbox2/dist/css/lightbox.min.css';
+// import 'lightbox2/dist/js/lightbox-plus-jquery.min.js';
+// import lightbox from 'lightbox2/dist/js/lightbox-plus-jquery.min.js';
+
 export default class Home extends Component {
 
     componentDidMount() {
+        // lightbox.option({
+        //     'showImageNumberLabel': false,
+        // })
         this.setSideColumnHeight();
         window.addEventListener('resize', this.setSideColumnHeight);
     }
@@ -24,6 +31,8 @@ export default class Home extends Component {
         contentContainer.style.height = window.innerHeight - contentTop - '50' + 'px';
         // audioCol.style.height = window.innerHeight - contentTop + 'px';
     }
+
+    // lightboxGalleryName = "home-slideshow";
 
     render() {
         return (
@@ -46,13 +55,13 @@ export default class Home extends Component {
                             {/* </div>
                         </div> */}
 
-
+{/* 
                         <div className="gallery-photos">
 
-                            {galleryPhotos.map(({ id, src, alt, caption }) => <a id={id} href={src.full} data-lightbox={this.lightboxGalleryName} data-title={caption}><img src={src.thumb} title={caption} alt={alt} /> </a>)}
+                            {galleryPhotos.map(({ id, src, alt, caption }) => <a id={id} rel='lightshow' href={src.full} data-lightbox={this.lightboxGalleryName} data-title={caption}><img src={src.thumb} title={caption} alt={alt} /> </a>)}
 
                         </div>
-                    
+ */}
                     </div>
                 </div>
 
