6ffc117071a925e4dda428447dc5091d3b68b62b
DC <david.chamberlin@tadashishoji.com>
2020-04-25 14:56:02 -0700

    fixed styling

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -69,12 +69,12 @@ export default class Home extends Component {
 
                     {/* experimenting with carousel */}
 
-                        <div id="demo" className="carousel slide" data-ride="carousel">
+                        <div id="home-carousel" className="carousel slide" data-ride="carousel">
 
                             {/* <!-- Indicators --> */}
                             <ul className="carousel-indicators">
-                                <li data-target="#demo" data-slide-to="0" className="active"></li>
-                                <li data-target="#demo" data-slide-to="1"></li>
+                                <li data-target="#home-carousel" data-slide-to="0" className="active"></li>
+                                <li data-target="#home-carousel" data-slide-to="1"></li>
                                 {/* <li data-target="#demo" data-slide-to="2"></li> */}
                             </ul>
 
@@ -86,16 +86,14 @@ export default class Home extends Component {
                                 <div className="carousel-item">
                                     <img src={testImg2} alt="test2"/>
                                 </div>
-                                {/* <div class="carousel-item">
-                                    <img src="ny.jpg" alt="New York"/>
-                                </div> */}
+
                             </div>
 
                             {/* <!-- Left and right controls --> */}
-                            <a className="carousel-control-prev" href="#demo" data-slide="prev">
+                            <a className="carousel-control-prev" href="#home-carousel" data-slide="prev">
                                 <span className="carousel-control-prev-icon"></span>
                             </a>
-                            <a className="carousel-control-next" href="#demo" data-slide="next">
+                            <a className="carousel-control-next" href="#home-carousel" data-slide="next">
                                 <span className="carousel-control-next-icon"></span>
                             </a>
 
