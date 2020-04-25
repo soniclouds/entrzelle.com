fe2f28100f73b15a458becb81a60d2736839b215
DC <david.chamberlin@tadashishoji.com>
2020-04-24 19:23:06 -0700

    finally decided on content/layout of home component -- render cta-icons (only) from connect component with unique top-bar display -- below, load image(s) from gallery -- currently loading all images, but want to implement full-size slideshow instead

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
+import CTA from './connect/cta.js';
+// import FB from './connect/fb.js';
+// import Gallery from './gallery.component.js';
+import galleryPhotos from './gallery/images.js';
+
+import '../assets/css/gallery.scss';
+import '../assets/css/connect.scss';
 import '../assets/css/home.scss';
 import '../assets/css/_content-component.scss';
 
@@ -20,10 +27,35 @@ export default class Home extends Component {
 
     render() {
         return (
-            <div id="ccc-outer" className="component-content-container--outer">
-                <div className="component-content-container--inner">
-                    home component
+            <div id="home-component">
+            
+                <div id="ccc-outer" className="component-content-container--outer">
+                    <div className="component-content-container--inner">
+                        {/* <div id="connect-component-container">
+                            <div id="connect--outer" className="--outer"> */}
+                                <div className="component-wrapper--cta">
+                                    <CTA />
+                                </div>
+
+                                {/* <div className="component-wrapper--fb"> */}
+                                    {/* <FB /> */}
+                                {/* </div> */}
+
+                                {/* <Gallery /> */}
+
+                            {/* </div>
+                        </div> */}
+
+
+                        <div className="gallery-photos">
+
+                            {galleryPhotos.map(({ id, src, alt, caption }) => <a id={id} href={src.full} data-lightbox={this.lightboxGalleryName} data-title={caption}><img src={src.thumb} title={caption} alt={alt} /> </a>)}
+
+                        </div>
+                    
+                    </div>
                 </div>
+
             </div>
         )
     }
