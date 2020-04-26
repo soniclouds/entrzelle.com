cf5fef2fd441b808b9516020ae030f3d9bd38fe6
DC <david.chamberlin@tadashishoji.com>
2020-04-25 18:47:31 -0700

    added copyright to footer -- footer is all set until media controls are implemented

--- src/components/footer.component.js
+++ src/components/footer.component.js
@@ -6,9 +6,11 @@ export default class Footer extends Component {
  
     render() {
         return (
-          <div id="footer-component">
-            footer here
-          </div>
+            <div id="footer-component">
+                <div className="copyright">
+                    <span>website by <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/soniclouds/">David Chamberlin</a> | &copy; 2020 Entrzelle  All Rights Reserved</span>
+                </div>
+            </div>
         )
     }
 }
