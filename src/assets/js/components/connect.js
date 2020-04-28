e8f986b7b6eb098df0a8755c422d2eafb76ed1a2
DC <david.chamberlin@tadashishoji.com>
2020-04-28 16:25:31 -0700

    added color indicator for connect toggle

--- src/assets/js/components/connect.js
+++ src/assets/js/components/connect.js
@@ -1,7 +1,8 @@
 function toggleConnectModal(e) {
 
     let outerConnectContainer = document.getElementById('connect-component-container'),
-        innerConnectContainer = document.getElementById('connect--outer');
+        innerConnectContainer = document.getElementById('connect--outer'),
+        connectToggle = document.getElementsByClassName('connect-toggle')[0];
 
     // when 'connect' nav link is clicked, toggle normally
     if (e.target.className === 'connect-toggle') {
@@ -11,20 +12,24 @@ function toggleConnectModal(e) {
             // outerConnectContainer.style.opacity = '0';
             setTimeout(function () { outerConnectContainer.style.opacity = '0'; }, 250);
             setTimeout(function () { outerConnectContainer.style.zIndex = '-1'; }, 500);
+            connectToggle.style.color = '#B8B8B8';
         } else {
             innerConnectContainer.style.height = '100%';
             innerConnectContainer.style.visibility = 'visible';
             outerConnectContainer.style.opacity = '0.93';
             // setTimeout(function () { outerConnectContainer.style.opacity = '0.93'; }, 500);
             outerConnectContainer.style.zIndex = '1';
+            connectToggle.style.color = 'white';
         }
     } else {
         // for all other links, toggle modal off
         if (innerConnectContainer.style.height !== '0%') {
             innerConnectContainer.style.height = '0%';
             innerConnectContainer.style.visibility = 'hidden';
-            outerConnectContainer.style.opacity = '0';
+            // outerConnectContainer.style.opacity = '0';
+            setTimeout(function () { outerConnectContainer.style.opacity = '0'; }, 250);
             setTimeout(function () { outerConnectContainer.style.zIndex = '-1'; }, 750);
+            connectToggle.style.color = '#B8B8B8';
         }
     }
 
